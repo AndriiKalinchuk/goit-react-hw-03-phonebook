aec09bf46ba4379c59a3238a526daa1fbece7e08
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-30 13:52:38 +0300

    DidMount_DidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,20 @@ export default class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     const { contacts } = this.state;
     const newContact = {
